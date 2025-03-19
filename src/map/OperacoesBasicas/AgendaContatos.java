0f85f3913df54e3baa808ba03cb786b3075dc122
Luiz Hélio <luizhelionerd@hotmail.com>
2025-03-19 14:52:53 -0300

    Create AgendaContatos.java

---
+++ src/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,34 @@
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+	private Map<String, Integer> agendaContatoMap;
+	
+	public AgendaContatos() {
+		this.agendaContatoMap = new HashMap<>();
+	}
+	
+	public void adicionarContato(String nome, Integer telefone) {
+		agendaContatoMap.put(nome, telefone);			//metodo "put" serve tanto para adicionar quanto atualizar -- faz o papel do "add" e do "set"
+	}
+	
+	public void removerContato(String nome) {
+		if(!agendaContatoMap.isEmpty()) {
+			agendaContatoMap.remove(nome);
+		}
+	}
+	
+	public void exibirContato() {
+		System.out.println(agendaContatoMap);
+	}
+	
+	public Integer pesquisarPorNome(String nome) {
+		Integer numeroPorNome = null;
+		if(!agendaContatoMap.isEmpty()) {
+			numeroPorNome = agendaContatoMap.get(nome);
+		}
+		return numeroPorNome;
+	}
+}
