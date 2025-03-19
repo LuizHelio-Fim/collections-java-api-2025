2ae904a349d9b7108303876af43db0c011c51a67
Luiz Hélio <luizhelionerd@hotmail.com>
2025-03-19 12:04:57 -0300

    Create CadastroProdutos.java

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,29 @@
+package set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+	private Set<Produto> produtoSet;
+
+	public CadastroProdutos() {
+		this.produtoSet = new HashSet<>();
+	}
+	
+	public void adicionarProduto(long cod, String nome, double preco, int quant) {
+		produtoSet.add(new Produto(cod, nome, preco, quant));
+	}
+	
+	public Set<Produto> exibirProdutoPorNome() {
+		Set<Produto> produtoPorNome = new TreeSet<>(produtoSet);
+		return produtoPorNome;
+	}
+	
+	public Set<Produto> exibirPorPreco() {
+		Set<Produto> produtoPorpreco = new TreeSet<>(new ComparatorPorPreco());
+		produtoSet.addAll(produtoSet);
+		return produtoPorpreco;
+	}
+	
+}
